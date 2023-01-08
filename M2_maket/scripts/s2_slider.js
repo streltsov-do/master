6f470a288db1aece40073ff827902cca3d27bb7b
Denis <streltsov.do@gmail.com>
2023-01-09 00:31:35 +0300

    m2: s2_slider - массив данных по изображениям, alt для img, изменение данных (city, area, time) слайда

--- M2_maket/scripts/s2_slider.js
+++ M2_maket/scripts/s2_slider.js
@@ -1,31 +1,45 @@
+///////////////////////////////////////////////////////////////////
+let s2_btn_dot_0    = document.querySelector(".s2_btn_dot_0");
+let s2_btn_dot_1    = document.querySelector(".s2_btn_dot_1");
+let s2_btn_dot_2    = document.querySelector(".s2_btn_dot_2");
+///////////////////////////////////////////////////////////////////
+let s2_btn_dsc_0    = document.querySelector(".s2_btn_desc_0");
+let s2_btn_dsc_1    = document.querySelector(".s2_btn_desc_1");
+let s2_btn_dsc_2    = document.querySelector(".s2_btn_desc_2");
+///////////////////////////////////////////////////////////////////
+let s2_h_city       = document.querySelector(".s2_heads_city");
+let s2_h_area       = document.querySelector(".s2_heads_area");
+let s2_h_time       = document.querySelector(".s2_heads_time");
+///////////////////////////////////////////////////////////////////
 let slider_assets= [
-    "images/s2_i1.png",
-    "images/s2_i2.png",
-    "images/s2_i3.png",
-]
+    {
+        img:    "images/s2_i1.png",
+        b_dot:  s2_btn_dot_0,
+        b_dsc:  s2_btn_dsc_0,
+        h_txt:  "Rostov-on-Don LCD Admiral",
+        h_city: "Rostov-on-Don </br> LCD Admiral",
+        h_area: "81 m2",
+        h_time: "3.5 months",
+    },
+    {
+        img:    "images/s2_i2.png",
+        b_dot:  s2_btn_dot_1,
+        b_dsc:  s2_btn_dsc_1,
+        txt:    "Sochi Thieves",
+        h_city: "Sochi </br> Thieves",
+        h_area: "105 m2",
+        h_time: "4 months",
+    },
+    {
+        img:    "images/s2_i3.png",
+        b_dot:  s2_btn_dot_2,
+        b_dsc:  s2_btn_dsc_2,
+        txt:    "Rostov-on-Don Patriotic",
+        h_city: "Rostov-on-Don </br> Patriotic",
+        h_area: "93 m2",
+        h_time: "3 months",
 
-// "url(../images/s2_i1.png)",
-// "url(../images/s2_i2.png)",
-// "url(../images/s2_i3.png)",
-///////////////////////////////////////////////////////////////////
-let s2_dot_0 = document.querySelector(".s2_btn_dot_0");
-let s2_dot_1 = document.querySelector(".s2_btn_dot_1");
-let s2_dot_2 = document.querySelector(".s2_btn_dot_2");
-
-let slider_dots= [
-    s2_dot_0,
-    s2_dot_1,
-    s2_dot_2,
-]
-///////////////////////////////////////////////////////////////////
-let s2_desc_0 = document.querySelector(".s2_btn_desc_0");
-let s2_desc_1 = document.querySelector(".s2_btn_desc_1");
-let s2_desc_2 = document.querySelector(".s2_btn_desc_2");
-
-let slider_desc= [
-    s2_desc_0,
-    s2_desc_1,
-    s2_desc_2,
+    },
 ]
 ///////////////////////////////////////////////////////////////////
 let s2_slide        = document.querySelector(".s2_slider");
@@ -33,7 +47,7 @@ let s2_slide        = document.querySelector(".s2_slider");
 let s2_slide_next = document.querySelector(".s2_btn_arrow_next");
 let s2_slide_prev = document.querySelector(".s2_btn_arrow_prev");
 ///////////////////////////////////////////////////////////////////
-let s2_slide_mobile = document.querySelector(".s2_hidden_prj_nav");
+let s2_slide_mobile = document.querySelector(".s2_hidden_slider");
 
 let s2_slide_mobile_next = document.querySelector(".s2_hidden_btn_next");
 let s2_slide_mobile_prev = document.querySelector(".s2_hidden_btn_prev");
@@ -42,8 +56,8 @@ let idx=0;
 let len=slider_assets.length;
 
 function slider(next) {
-    slider_dots[idx].innerHTML = '<img class="s2_dot">';
-    slider_desc[idx].classList.remove('s2_btn_desc_selected');
+    slider_assets[idx].b_dot.innerHTML = '<img class="s2_dot">';
+    slider_assets[idx].b_dsc.classList.remove('s2_btn_desc_selected');
 
     if (next) {
         if ((idx+1)==len) {
@@ -60,23 +74,29 @@ function slider(next) {
     }
     // console.log("idx",idx);
 
-    s2_slide.style.setProperty("background-image",`url(${slider_assets[idx]})`);
-    slider_dots[idx].innerHTML = '<img class="s2_dot s2_dot_selected">';
-    slider_desc[idx].classList.add('s2_btn_desc_selected');
-    s2_slide_mobile.style.setProperty("background-image",`url(${slider_assets[idx]})`);
+    slider_assets[idx].b_dot.innerHTML = '<img class="s2_dot s2_dot_selected">';
+    slider_assets[idx].b_dsc.classList.add('s2_btn_desc_selected');
+    s2_slide.innerHTML = `<img class="s2_slider_img" src="${slider_assets[idx].img}" alt="${slider_assets[idx].txt}"> <img>`;
+    s2_slide_mobile.innerHTML = `<img class="s2_slider_img_mobile" src="${slider_assets[idx].img}" alt="${slider_assets[idx].txt}"> <img>`;
+    s2_h_city.innerHTML = slider_assets[idx].h_city;
+    s2_h_area.innerHTML = slider_assets[idx].h_area;
+    s2_h_time.innerHTML = slider_assets[idx].h_time;
 }
 
 
 function slider_i(i) {
-    slider_dots[idx].innerHTML = '<img class="s2_dot">';
-    slider_desc[idx].classList.remove('s2_btn_desc_selected');
+    slider_assets[idx].b_dot.innerHTML = '<img class="s2_dot">';
+    slider_assets[idx].b_dsc.classList.remove('s2_btn_desc_selected');
 
     idx=i;
-    s2_slide.style.setProperty("background-image",`url(${slider_assets[idx]})`);
-    s2_slide_mobile.style.setProperty("background-image",`url(${slider_assets[idx]})`);
     
-    slider_dots[idx].innerHTML = '<img class="s2_dot s2_dot_selected">';
-    slider_desc[idx].classList.add('s2_btn_desc_selected');
+    slider_assets[idx].b_dot.innerHTML = '<img class="s2_dot s2_dot_selected">';
+    slider_assets[idx].b_dsc.classList.add('s2_btn_desc_selected');
+    s2_slide.innerHTML = `<img class="s2_slider_img" src="${slider_assets[idx].img}" alt="${slider_assets[idx].txt}"> <img>`;
+    s2_slide_mobile.innerHTML = `<img class="s2_slider_img_mobile" src="${slider_assets[idx].img}" alt="${slider_assets[idx].txt}"> <img>`;
+    s2_h_city.innerHTML = slider_assets[idx].h_city;
+    s2_h_area.innerHTML = slider_assets[idx].h_area;
+    s2_h_time.innerHTML = slider_assets[idx].h_time;
 }
 ///////////////////////////////////////////////////////////////////
 s2_slide_mobile_next.addEventListener("click", () => { 
@@ -95,22 +115,22 @@ s2_slide_prev.addEventListener("click", () => {
     slider(false)
 });
 ///////////////////////////////////////////////////////////////////
-s2_dot_0.addEventListener("click", () => { 
+s2_btn_dot_0.addEventListener("click", () => { 
     slider_i(0)
 });
-s2_dot_1.addEventListener("click", () => { 
+s2_btn_dot_1.addEventListener("click", () => { 
     slider_i(1)
 });
-s2_dot_2.addEventListener("click", () => { 
+s2_btn_dot_2.addEventListener("click", () => { 
     slider_i(2)
 });
 
-s2_desc_0.addEventListener("click", () => { 
+s2_btn_dsc_0.addEventListener("click", () => { 
     slider_i(0)
 });
-s2_desc_1.addEventListener("click", () => { 
+s2_btn_dsc_1.addEventListener("click", () => { 
     slider_i(1)
 });
-s2_desc_2.addEventListener("click", () => { 
+s2_btn_dsc_2.addEventListener("click", () => { 
     slider_i(2)
 });
